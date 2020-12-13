1bdd2bf3d5826e9769722fc5dac24b12b300dc1d
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2020-12-13 09:06:10 +0000

    Add error; add error handeling

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
+from discord import client
 from discord.ext import commands
+import asyncio
 
 import urllib.request
 import json
@@ -15,6 +17,11 @@ bot = commands.Bot(command_prefix='^')
 branch = "main"
 latestTree = {}
 
+# Errors
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send("❌ Unhandled error: `"+error+"`")
+
 # Bot command declarations
 @bot.command()
 async def foo(ctx, arg):
@@ -25,12 +32,21 @@ async def ping(ctx):
     await ctx.send('Pong! Latency: `'+str(round(bot.latency*1000))+"ms`")
 
 @bot.group()
+@commands.is_owner()
 async def admin(ctx):
     if ctx.invoked_subcommand is None:
-        await ctx.send("Subcommand pls.")
+        await ctx.message.add_reaction("❌")
+        await ctx.send("❌ **Incorrect or missing subcommand.**")
+
+@admin.error
+async def admin_error(ctx, error):
+    if isinstance(error, discord.ext.commands.errors.NotOwner):
+        await ctx.message.add_reaction("❌")
+        await ctx.send("❌ **You do not have permission.**")
 
 @admin.command()
 async def disconnect(ctx):
+    await ctx.channel.send("Goodbye.")
     await bot.close()
 
 @bot.event
